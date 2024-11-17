1595e3ee53ad910fca61381a899dea0f0430f431
phizura <satrioapra@gmail.com>
2024-11-17 17:10:16 +0700

    style(main-screen): append padding widget

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -13,52 +13,55 @@ class MainScreen extends StatelessWidget {
           foregroundColor: Colors.white,
           title: const Text('Wisata Pasuruan'),
         ),
-        body: ListView.builder(
-            itemCount: tourismPlaceList.length,
-            itemBuilder: (context, index) {
-              final TourismPlace place = tourismPlaceList[index];
-              return InkWell(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => DetailScreen(place: place)));
-                },
-                child: Card(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(
-                          flex: 1,
-                          child: ClipRRect(
-                            borderRadius: const BorderRadius.only(
-                                topLeft: Radius.circular(12),
-                                bottomLeft: Radius.circular(12)),
-                            child: Image.asset(place.imageAsset),
-                          )),
-                      Expanded(
-                          flex: 1,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Text(
-                                  place.name,
-                                  style: const TextStyle(fontSize: 16.0),
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Text(place.location)
-                              ],
-                            ),
-                          ))
-                    ],
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView.builder(
+              itemCount: tourismPlaceList.length,
+              itemBuilder: (context, index) {
+                final TourismPlace place = tourismPlaceList[index];
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => DetailScreen(place: place)));
+                  },
+                  child: Card(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                            flex: 1,
+                            child: ClipRRect(
+                              borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(12),
+                                  bottomLeft: Radius.circular(12)),
+                              child: Image.asset(place.imageAsset),
+                            )),
+                        Expanded(
+                            flex: 1,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Text(
+                                    place.name,
+                                    style: const TextStyle(fontSize: 16.0),
+                                  ),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  Text(place.location)
+                                ],
+                              ),
+                            ))
+                      ],
+                    ),
                   ),
-                ),
-              );
-            }));
+                );
+              }),
+        ));
   }
 }
