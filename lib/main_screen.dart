47f2d392e9944e917b62a2048c4f52b4f5ef16bd
phizura <satrioapra@gmail.com>
2024-11-17 16:35:58 +0700

    feat(main-screen): make dinamic data

---
+++ lib/main_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/detail_screen.dart';
+import 'package:travel_app/model/tourism_place.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 126, 118, 118),
+          foregroundColor: Colors.white,
+          title: const Text('Wisata Pasuruan'),
+        ),
+        body: ListView.builder(
+            itemCount: tourismPlaceList.length,
+            itemBuilder: (context, index) {
+              final TourismPlace place = tourismPlaceList[index];
+              return InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => DetailScreen(place: place)));
+                },
+                child: Card(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                          flex: 1,
+                          child: ClipRRect(
+                            borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(12),
+                                bottomLeft: Radius.circular(12)),
+                            child: Image.asset(place.imageAsset),
+                          )),
+                      Expanded(
+                          flex: 1,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  place.name,
+                                  style: const TextStyle(fontSize: 16.0),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Text(place.location)
+                              ],
+                            ),
+                          ))
+                    ],
+                  ),
+                ),
+              );
+            }));
+  }
+}
